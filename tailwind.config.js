b71e482773948b9810fb7ee01531f91db98611b7
f3rc0n5 <ferdynandergypramudani11@gmail.com>
2025-12-11 18:37:05 +0700

    add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px"
+    },
     extend: {
       colors: {
         primaryColor: "#010a5e",
@@ -16,6 +21,17 @@ module.exports = {
         darkColorLight: "#171717",
       },
     },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1.5rem",
+      },
+    },
+    fontFamily: {
+      oswald: ['Oswald','sans-serif'],
+      dmsans: ['DM Sans', 'sans-serif']
+    }
   },
   plugins: [],
 };
