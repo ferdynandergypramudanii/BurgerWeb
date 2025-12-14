a005d9c3e4983cbb641ab6dc4d7ed0a6282dd5f7
f3rc0n5 <ferdynandergypramudani11@gmail.com>
2025-12-14 14:21:59 +0700

    add dark mode

--- src/main.js
+++ src/main.js
@@ -17,81 +17,103 @@ hamburger.addEventListener("click", () => {
   navMenu.classList.remove("hidden");
 });
 
-
 // TABS
 
 const tabs = document.querySelectorAll(".tabs_wrap ul li");
-const all = document.querySelectorAll('.item_wrap');
-const foods = document.querySelectorAll('.food');
-const beverages = document.querySelectorAll('.beverage');
-const snacks = document.querySelectorAll('.snack');
+const all = document.querySelectorAll(".item_wrap");
+const foods = document.querySelectorAll(".food");
+const beverages = document.querySelectorAll(".beverage");
+const snacks = document.querySelectorAll(".snack");
 
-
-tabs.forEach(tab => {
+tabs.forEach((tab) => {
   tab.addEventListener("click", () => {
-    tabs.forEach(tab => {
-      tab.classList.remove("active")
-    })
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
 
-    tab.classList.add('active')
+    tab.classList.add("active");
 
-    const tabVal = tab.getAttribute('data-tabs')
+    const tabVal = tab.getAttribute("data-tabs");
     // console.log(tabval)
 
-    all.forEach(item => {
-      item.style.display = 'none'
-    })
+    all.forEach((item) => {
+      item.style.display = "none";
+    });
 
-    if (tabVal == 'food') {
-      foods.forEach(item => {
-        item.style.display = 'block'
-     })
-    
+    if (tabVal == "food") {
+      foods.forEach((item) => {
+        item.style.display = "block";
+      });
+    } else if (tabVal == "snack") {
+      snacks.forEach((item) => {
+        item.style.display = "block";
+      });
+    } else if (tabVal == "beverage") {
+      beverages.forEach((item) => {
+        item.style.display = "block";
+      });
+    } else {
+      all.forEach((item) => {
+        item.style.display = "block";
+      });
     }
-    else if (tabVal == 'snack') {
-      snacks.forEach(item => {
-        item.style.display = 'block'
-      }) 
-    }
-    else if (tabVal == 'beverage') {
-      beverages.forEach(item => {
-        item.style.display = 'block'
-      }) 
-    }
-    else {
-      all.forEach(item => {
-        item.style.display = 'block'
-      })
-    }
-  })
-})
+  });
+});
 
+// Dark Light Theme
+const html = document.querySelector("html");
+const themeBtn = document.getElementById("theme-toggle");
 
+if (localStorage.getItem("mode") == "dark") {
+  darkMode();
+} else {
+  lightMode();
+}
+
+themeBtn.addEventListener("click", (e) => {
+  if (localStorage.getItem("mode") == "light") {
+    darkMode();
+  } else {
+    lightMode();
+  }
+});
+
+function darkMode() {
+  html.classList.add("dark");
+  themeBtn.classList.replace("ri-moon-line", "ri-sun-line");
+  localStorage.setItem("mode", "dark");
+};
+
+function lightMode() {
+  html.classList.remove("dark");
+  themeBtn.classList.replace("ri-sun-line", "ri-moon-line");
+  localStorage.setItem("mode", "light");
+};
 
 // Show Scroll UP
 const scrollUp = () => {
   const scrollUpBtn = document.getElementById("scroll-up");
 
-  if(this.scrollY > 250) {
+  if (this.scrollY > 250) {
     scrollUpBtn.classList.remove("-bottom-1/2");
     scrollUpBtn.classList.add("bottom-4");
   } else {
     scrollUpBtn.classList.add("-bottom-1/2");
     scrollUpBtn.classList.remove("bottom-4");
   }
-}
+};
 
-window.addEventListener('scroll', scrollUp)
+window.addEventListener("scroll", scrollUp);
 
 // Show Scroll UP
 const scrollHeader = () => {
   const header = document.getElementById("header");
 
-  if(this.scrollY >= 50) {
+  if (this.scrollY >= 50) {
     header.classList.add("border-b", "border-secondaryColor");
   } else {
     header.classList.remove("border-b", "border-secondaryColor");
   }
-}
+};
 
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener("scroll", scrollHeader);
